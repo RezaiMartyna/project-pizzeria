b203fe888ebb72a49a0951257db06e346b5a57a6
Martyna Rezai <martyna.rezai@gmail.com>
2021-01-05 21:57:39 +0100

    heroku

--- src/js/settings.js
+++ src/js/settings.js
@@ -120,7 +120,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
@@ -128,7 +128,7 @@ export const settings = {
     dateStartParamKey: 'date_gte',
     dateEndParamKey: 'date_lte',
     notRepeatParam: 'repeat=false',
-    repeatParam: 'repeat_ne=false',
+    repeatParam: 'Yrepeat_ne=false',
   },
 
 };
